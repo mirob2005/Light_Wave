dfe2f9c727d47d4d3c731c51209524a5a965b5cd
Michael Robertson <mirob2005@gmail.com>
2012-05-23 16:15:29 -0700

    Modified commandline inputs to only require scene file name, other files are automatically generated from it... Change Light type in scene file to DLight

--- raytrace/raytrace.cpp
+++ raytrace/raytrace.cpp
@@ -53,9 +53,9 @@ float H = 0;
 
 void usage( string message = "" ){
 	cerr << message << endl;
-	cerr << gProgramName << " -i <inputfile> -o <outputfile> -d <depthfile>" << endl;
+	cerr << gProgramName << " -i <inputfile>" << endl;
 	cerr << "          -or-" << endl;
-	cerr << gProgramName << " --input <inputfile> --output <outputfile> --depth <depthfile>" << endl;
+	cerr << gProgramName << " --input <inputfile>" << endl;
 }
 
 std::ostream& operator <<( std::ostream &out, const Pixel &p ){
@@ -69,8 +69,6 @@ void parseCommandLine( int argc, char **argv ){
 
 	static struct option longopts[] = {
 		{ "input", required_argument, NULL, 'i' },
-		{ "output", required_argument, NULL, 'o' },
-		{ "depth", required_argument, NULL, 'd' },
 		{ "resolution", required_argument, NULL, 'r' },
 		{ "verbose", required_argument, NULL, 'v' },
 		{ "help", required_argument, NULL, 'h' },
@@ -78,19 +76,11 @@ void parseCommandLine( int argc, char **argv ){
 		{ NULL, 0, NULL, 0 }
 	};
 
-	while( (ch = getopt_long(argc, argv, "i:o:d:r:svh", longopts, NULL)) != -1 ){
+	while( (ch = getopt_long(argc, argv, "i:r:svh", longopts, NULL)) != -1 ){
 		switch( ch ){
 			case 'i':
 				// input file
 				inputFile = string(optarg);
-				break;
-			case 'o':
-				// image output file
-				outputFile = string(optarg);
-				break;
-			case 'd':
-				// depth output file
-				depthFile = string( optarg );
 				break;
 			case 'r':
 				gResolution = atoi(optarg);
@@ -109,6 +99,11 @@ void parseCommandLine( int argc, char **argv ){
 				break;
 		}
 	}
+	// Generate File Names for color image and depth image output files from the scene input file
+	string temp = inputFile;
+	string temp2 = inputFile;
+	outputFile = temp.replace(inputFile.length()-4,4,"").append("_color_image.ppm");
+	depthFile = temp2.replace(inputFile.length()-4,4,"").append("_depth_image.ppm");
 	gTheScene = new Scene( inputFile, outputFile, depthFile );
 }
 
